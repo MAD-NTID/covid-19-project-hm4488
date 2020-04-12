facdfb2e80cc9f2cb68ad5cba29650d48add5bd2
Harsh Mathur <46754500+hm4488@users.noreply.github.com>
2020-04-12 18:44:42 -0400

    Repeated Same thing
    DONE

---
+++ CovidApp/CovidApp/CovidApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CovidApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            SurvivalGuideCollection.PopulateData();
+
+            MainPage = new SurvivalGuidePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CovidApp/CovidApp/CovidApp/SurvivalGuideCollection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CovidApp
+{
+    public static class SurvivalGuideCollection
+    {
+        public static ObservableCollection<Guide> Guides { set; get; }
+
+        public static void PopulateData()
+        {
+            Guides = new ObservableCollection<Guide>
+            {
+                new Guide(){Info = "Clean your hands!!", Details = "Regularly and thoroughly clean your hands with an alcohol-based hand rub or wash them with soap and water."},
+                new Guide() {Info = "Avoid close contact!!", Details = "Avoid close contatc with people who are sick, maintain 3 feet/6 meters away from other people."},
+                new Guide() {Info = "Cover your cough!!", Details="Cover your nose and mouth with a disposable tissue or flexed elbow when you cough or sneeze"},
+                new Guide(){Info = "Avoid touching eyes, nose and mouth", Details="Hands touch many surfaces and can pick up viruses. Once contaminated, hands can transfer the virus to your eyes, nose or mouth."},
+                new Guide(){Info="If you have fever, cough and difficulty breathing, seek medical care early", Details="Stay home if you feel unwell. If you have a fever, cough and difficulty breathing, seek medical attention and call in advance."},
+                new Guide(){Info="Try to stay home", Details="Staying at home reduces the spread of coronavirus and reduces the risk of getting infected as well as spread to other people"}
+
+
+            };
+        }
+
+        public class Guide
+        {
+            public string Info { get; set; }
+            //public Uri ImageUrl { get; set; }
+            public string Details { get; set; }
+
+
+        }
+    }
+}
