6faf59c389edba0dda56e18cc3677de273f9f0d6
Harsh Mathur <46754500+hm4488@users.noreply.github.com>
2020-04-13 06:30:14 -0400

    Lottie Successful
    Many failures are happening before one success exists!!

--- CovidApp/CovidApp/CovidApp.Android/MainActivity.cs
+++ CovidApp/CovidApp/CovidApp.Android/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Lottie.Forms;
+using Lottie.Forms.Droid;
 
 namespace CovidApp.Droid
 {
@@ -21,6 +23,7 @@ namespace CovidApp.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            AnimationViewRenderer.Init();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
